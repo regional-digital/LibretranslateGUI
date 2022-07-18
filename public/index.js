dad519dfbe31e5c21c47f0fcfba7e3e06eca147a
Alexander Gabriel <alexander.gabriel@digital-infinity.de>
2022-07-18 20:32:01 +0200

    first try with keyboard

--- public/index.js
+++ public/index.js
@@ -1,4 +1,52 @@
+function generateKeyboard(layout = false) {
+  const Keyboard = window.SimpleKeyboard.default;
+  const defaultTheme = "hg-theme-default";
+
+  if(layout) {
+    const KeyboardLayouts = window.SimpleKeyboardLayouts.default;
+    const layout = new KeyboardLayouts().get(layout);
+    const myKeyboard = new Keyboard({
+      onChange: input => onChange(input),
+      onKeyPress: button => onKeyPress(button),
+      ...layout
+    });
+  } else {
+    const myKeyboard = new Keyboard({
+      onChange: input => onChange(input),
+      onKeyPress: button => onKeyPress(button)
+    });
+  }
+  
+  function onChange(input) {
+    document.querySelector(".input").value = input;
+    console.log("Input changed", input);
+  }
+  
+  function onKeyPress(button) {
+    console.log("Button pressed", button);
+  }
+  
+  function showKeyboard() {
+    console.log("showKeyboard");
+    myKeyboard.setOptions({
+      theme: `${defaultTheme} show-keyboard`
+    });
+  }
+  
+  function hideKeyboard() {
+    console.log("hideKeyboard");
+    myKeyboard.setOptions({
+      theme: defaultTheme
+    });
+  }
+}
+
+generateKeyboard();
+hideKeyboard();
+
 $(document).ready(function(){
+
+
   $.get("/languages",
   function(data) {
     var sel = $("#sourceLanguage");
@@ -31,4 +79,19 @@ $(document).ready(function(){
     });
   });
 
+  $('#keyboardSelect').on('change', function() {
+    keyboardSelectValue = $('#keyboardSelect').val();
+    console.log("keyboardSelect changed: "+keyboardSelectValue);
+    if(keyboardSelectValue == "off") {
+      hideKeyboard();
+    }
+    else if (keyboardSelectValue == "default") {
+      hideKeyboard();
+      generateKeyboard();
+    }
+    else {
+      hideKeyboard();
+      generateKeyboard(keyboardSelectValue);
+    }
+  });
 });
